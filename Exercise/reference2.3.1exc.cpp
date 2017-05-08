58dc258e0731c349b969f26d07ed7380035bb458
Ulez <1104128773@qq.com>
2017-05-08 10:11:55 +0800

    引用；

---
+++ Exercise/reference2.3.1exc.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+//引用不是一个对象，而是一个已经存在的对象所起的别名，必须初始化。
+int main(){
+  int i,&ri=i;
+  i=5;ri=10;
+  std::cout<<i<< ":" <<ri<<std::endl;
+  getchar();
+  return 0;
+}
--- Exercise/源.cpp
+++ Exercise/源.cpp
@@ -9,14 +9,14 @@ struct stu    // 定义一个结构体
     int num;  // 学号
     int age;  // 年龄
 };
-int main()
-{
-	string name="姓名" ;  // 姓名
-    struct stu *s =new stu;   // 定义一个结构体指针
-    s->name = name;                    
-    s->num = 2015120; 
-    s->age = 18;      
-    cout << "age="<< s->age<< "," <<"\n"<< "num="<< s->num  << ","<< "\n"<<"name=" << s->name << endl;
-    getchar();
-    return 0;
-}
+//int main()
+//{
+//	string name="姓名" ;  // 姓名
+//    struct stu *s =new stu;   // 定义一个结构体指针
+//    s->name = name;                    
+//    s->num = 2015120; 
+//    s->age = 18;      
+//    cout << "age="<< s->age<< "," <<"\n"<< "num="<< s->num  << ","<< "\n"<<"name=" << s->name << endl;
+//    getchar();
+//    return 0;
+//}
